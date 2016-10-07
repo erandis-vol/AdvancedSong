97c32f58dd1cf0f65da24e569ea65420911f7de8
doom-desire <jack.bulan@gmail.com>
2016-10-07 19:53:43 -0400

    Find song table
    The program can search a ROM for the M4A engine

--- source/app.d
+++ source/app.d
@@ -2,10 +2,45 @@ import asong.assembler;
 import asong.rom;
 import std.stdio;
 
+int findSongTable(ROM rom)
+{
+	// bluh
+	ubyte[] code = [
+		0x00, 0xB5, 0x00, 0x04, 0x07, 0x4A, 0x08, 0x49,
+		0x40, 0x0B, 0x40, 0x18, 0x83, 0x88, 0x59, 0x00,
+		0xC9, 0x18, 0x89, 0x00, 0x89, 0x18, 0x0A, 0x68,
+		0x01, 0x68, 0x10, 0x1C, 0x00, 0xF0
+	];
+
+	// find select song function
+	int selectSongOffset = rom.findLoose(code, 8);
+	if (selectSongOffset == -1)
+		return -1;
+
+	// read pointer to song table
+	rom.seek(selectSongOffset + 0x28);
+	int songTablePtr = rom.readInt32();
+
+	// if it was actually a pointer, return offset
+	if (songTablePtr >= 0x8000000 && songTablePtr <= 0x9FFFFFF)
+		return songTablePtr & 0x1FFFFFF;
+
+	// otherwise failure
+	return -1;
+}
+
+
 void main()
 {
-	writeln("Doing things to Po Pi Po.s...");
+	writeln("Opening \"bpre.gba\".");
 	auto rom = new ROM("bpre.gba");
-	auto a = new Assembler(rom);
-	a.assemble("Po Pi Po.s", 0x900000, 0x800000, 0x123456);
+	int songTable = rom.findSongTable();
+	if (songTable == -1)
+		writeln("Song table not found!");
+	else
+		writefln("Song table at: 0x%07X", songTable);
+
+	//writeln("Doing things to Po Pi Po.s...");
+	//auto a = new Assembler(rom);
+	//a.assemble("Po Pi Po.s", 0x900000, 0x800000, 0x48ABB0);
 }
--- source/rom.d
+++ source/rom.d
@@ -30,8 +30,14 @@ public:
 
 	void seek(int offset)
 	{
-		assert(offset > 0 && offset < buffer.length, "Cannot seek beyond file.!");
 		p = offset;
+		assert(p > 0 && p < buffer.length, "Cannot seek beyond file.!");
+	}
+
+	void skip(int bytes)
+	{
+		p += bytes;
+		assert(p > 0 && p < buffer.length, "Cannot seek beyond file.!");
 	}
 
 	ubyte readUByte()
@@ -39,13 +45,35 @@ public:
 		return buffer[p++];
 	}
 
+	ubyte peekUByte()
+	{
+		return buffer[p];
+	}
+
 	int readInt32()
 	{
 		int i;
-		for (int j = 0; j < 4; j++)
-			i |= readUByte() << (j * 8);
-
+		for (int j = 0; j < 4; j++) {
+			i |= buffer[p++] << (j * 8);
+		}
 		return i;
+	}
+
+	int peekInt32()
+	{
+		int i;
+		for (int j = 0; j < 4; j++) {
+			i |= buffer[p + j] << (j * 8);
+		}
+		return i;
+	}
+
+	ubyte[] readUBytes(int count)
+	{
+		ubyte[] r = new ubyte[count];
+		for (int i = 0; i < count; i++)
+			r[i] = buffer[p++];
+		return r;
 	}
 
 	int readPointer()
@@ -57,6 +85,19 @@ public:
 
 		return ptr & 0x1FFFFFF;
 	}
+
+	/*int peekPointer()
+	{
+		int ptr = peekInt32();
+
+		if (ptr == 0)
+			return 0;
+
+		if (ptr >= 0x8000000 && ptr <= 0x9FFFFFF)
+			return ptr & 0x1FFFFFF;
+
+		return -1;
+	}*/
 
 	void writeUByte(ubyte value)
 	{
@@ -77,6 +118,33 @@ public:
 			writeInt32(offset | 0x8000000);
 		else
 			writeInt32(0);
+	}
+
+	int findLoose(ubyte[] search, int maxDiff)
+	{
+		int p = 0;
+
+		int compareLoose()
+		{
+			if (maxDiff <= 0)
+				return 0;
+
+			int d = 0;
+			for (int i = 0; i < search.length; i++) {
+				if (buffer[i + p] != search[i]) {
+					if (++d >= maxDiff)
+						return d;
+				}
+			}
+			return d;
+		}
+
+		for (; p < buffer.length - search.length; p += 4) {
+			if (compareLoose() < 8)
+				return p;
+		}
+
+		return -1;
 	}
 
 	@property int position() const
